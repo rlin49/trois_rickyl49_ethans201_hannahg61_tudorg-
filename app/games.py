8cc15cb8639d1a5ddc2396fccb918c03cc8327c0
Tudor Ganshaw <tganshaw@gmail.com>
2026-03-30 22:11:47 +0000

    updating database and whatnot

--- app/games.py
+++ app/games.py
@@ -2,14 +2,49 @@ import sqlite3
 
 DB_NAME = "Data/database.db"
 
-int get_id(game_name):
+def get_id(game_name): # TESTED
     DB = sqlite3.connect(DB_NAME)
     DBC = DB.cursor()
 
-    DBC.execute("SELECT id FROM games WHERE name = ?;", (game_name, ))
+    DBC.execute("SELECT id FROM games WHERE LOWER(name) LIKE LOWER(?);", (game_name, ))
     DBCF = DBC.fetchone()
     if DBCF is None:
         return -1
     DB.commit()
     DB.close()
     return DBCF[0]
+    
+def get_name(game_id): # TESTED
+    DB = sqlite3.connect(DB_NAME)
+    DBC = DB.cursor()
+    
+    DBC.execute("SELECT name FROM games WHERE id = ?;", (game_id, ))
+    DBCF = DBC.fetchone()
+    if DBCF is None:
+        return -1
+    DB.commit()
+    DB.close()
+    return DBCF[0]
+    
+def get_rating(game_info):
+    DB = sqlite3.connect(DB_NAME)
+    DBC= DB.cursor()
+    
+    if isInstance(game_info, int):
+        DBC.execute("SELECT user_rating FROM games WHERE id = ?;", (game_info, ))
+    else:
+        DBC.execute("SELECT user_rating FROM games WHERE name = ?;", (game_info, ))
+        
+    DBCF= DBC.fetchone()
+    if DBCF is None:
+        return -1
+    DB.commit()
+    DB.close()
+    return DBCF[0]
+    
+def add_rating(game_info):
+    DB = sqlite3.connect(DB_NAME)
+    DBC= DB.cursor()
+    
+    
+    
