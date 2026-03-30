23b8b5c65cbb379111fbea93b2794eb156d3fd91
Ethan Saldanha <ethans201@nycstudents.net>
2026-03-30 11:01:21 -0400

    merge

--- app/__init__.py
+++ app/__init__.py
@@ -13,6 +13,8 @@ DBC.execute("CREATE TABLE IF NOT EXISTS users(username TEXT, password TEXT, revi
 
 DBC.execute("CREATE TABLE IF NOT EXISTS reviews(game_id INT, body TEXT, user_id INT, id INTEGER PRIMARY KEY AUTOINCREMENT);")
 
+app.secret_key = "secret_key_testing"
+
 @app.route("/")
 def main():
     return "P04 Temp Site"
@@ -48,6 +50,42 @@ def homepage():
         return redirect(url_for('login'))
     else:
         return render_template('homepage.html',error="")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+  if request.method == "POST":
+    username = request.form.get("username", "").strip()
+    email = request.form.get("email", "").strip()
+    password = request.form.get("password", "")
+    confirm = request.form.get("confirm", "")
+    reviews=""
+    if not username or not email or not password or not confirm:
+      return render_template("register.html", error="All fields are required!")
+
+    if password != confirm:
+      return render_template("register.html", error="Passwords do not match!")
+
+
+
+    db = sqlite3.connect(DB_NAME)
+    c = db.cursor()
+
+    c.execute("SELECT * FROM users WHERE username = ?", (username,))
+    if c.fetchone():
+      db.close()
+      return render_template("register.html", error="Username already taken!")
+
+    c.execute("INSERT INTO users VALUES (?, ?, ?, ?)",
+    (username, password, reviews, 0))
+
+    db.commit()
+    db.close()
+
+    session['username'] = username
+    return redirect(url_for("homepage"))
+
+  return render_template("register.html")
 
 if __name__ == "__main__":
     app.debug = True
