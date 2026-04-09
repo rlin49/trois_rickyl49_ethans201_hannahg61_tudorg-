cd5913c4836533a4a0419ba1907d8c11822205e2
tganshaw <tudorganshaw@gmail.com>
2026-04-09 12:17:37 -0400

    Got game page up and running

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import *
-import os, json, sqlite3
+import os, json, sqlite3, random
 
 app = Flask(__name__)
 
@@ -38,12 +38,60 @@ def homepage():
         return render_template('homepage.html',username=username, error="")
 
 
-@app.route("/game")
+@app.route("/game", methods = ["GET", "POST"])
 def game():
     if 'username' not in session:
         return redirect(url_for('login'))
     else:
-        return render_template('game.html')
+        json_file = open("Data/games.json", "r")
+        data = json.load(json_file)
+        data_keys = list(data.keys())
+        guessed_amt = ""
+        real_amt = ""
+        game_arr = ""
+
+        if "game_index" in request.form:
+            check_name = data_keys[int(request.form["game_index"])]
+            check_dict = data[check_name]
+
+            game_arr = request.form["game_arr"].split(";")
+            guessed_amt = request.form["guess_arr"].split(";")
+            real_amt = request.form["ans_arr"].split(";")
+
+            game_arr.append(str(int(request.form["game_index"])))
+            guessed_amt.append(str(round(float(request.form["guessed_sales"]), 4)))
+            real_amt.append(str(round(float(check_dict["Global_Sales"]), 4)))
+
+
+            guessed_amt = ";".join(guessed_amt)
+            real_amt = ";".join(real_amt)
+            game_arr = ";".join(game_arr)
+
+            if guessed_amt[0] == ";":
+                guessed_amt = guessed_amt[1:]
+            if real_amt[0] == ";":
+                real_amt = real_amt[1:]
+            if game_arr[0] == ";":
+                game_arr = game_arr[1:]
+
+
+
+        game_index = random.randint(0, len(data_keys))
+        game_name = data_keys[game_index]
+        info_dict = data[game_name]
+
+
+        sales_rank = info_dict["Rank"]
+        platforms = info_dict["Platform"]
+        year = info_dict["Year"]
+        genre = info_dict["Genre"]
+        publisher = info_dict["Publisher"]
+        public_rating = info_dict["public_rating"]
+        if public_rating == -1:
+            public_rating = "No Metacritic Score was Available"
+        description = info_dict["description"]
+
+        return render_template('game.html', guess_arr = guessed_amt, ans_arr = real_amt, game_arr = game_arr, game_name = game_name, game_index = game_index, rank = sales_rank, platform = platforms, year = year, genre = genre, publisher = publisher, rating = public_rating, description = description)
 
 @app.route("/profile/<username>")
 def profile(username):
@@ -71,7 +119,7 @@ def registerhtml():
         return redirect("/")
     return render_template("register.html")
 
-@app.route("/login", methods=["GET", "POST"])
+@app.route("/login", methods = ["GET", "POST"])
 def login():
   if 'username' in session:
       return redirect(url_for('homepage'))
